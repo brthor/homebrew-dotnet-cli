00c932ad053a6d25f2e151050945edb21acf35c7
Bryan Thornbury <brthor@microsoft.com>
2016-07-20 19:04:06 -0700

    add SxS capability

--- Formula/dotnetcli.rb
+++ Formula/dotnetcli.rb
@@ -13,20 +13,23 @@ class Dotnetcli < Formula
     system "git remote add -t rel/1.0.0-preview2 -f origin https://github.com/dotnet/cli.git"
     system "git checkout -f rel/1.0.0-preview2"
     system "./build.sh", "--targets", "Prepare,Compile"
+    system "mkdir -p /usr/local/share/dotnet"
     system "cp -n ./artifacts/*/stage2/dotnet /usr/local/share/dotnet/dotnet"
     system "cp -R -n ./artifacts/*/stage2/shared/. /usr/local/share/dotnet/shared"
-    system "cp -R -n ./artifacts/*/stage2/sdk/. /usr/local/share/dotnet/shared"
-    system "ln -s /usr/local/share/dotnet #{bin}/dotnet"
+    system "cp -R -n ./artifacts/*/stage2/sdk/. /usr/local/share/dotnet/sdk"
+    system "cp -R -n ./artifacts/*/stage2/host/. /usr/local/share/dotnet/host"
+    system "mkdir -p #{bin}"
+    system "ln -s /usr/local/share/dotnet/dotnet #{bin}/dotnet"
   end
 
   test do
     system "#{bin}/dotnet", "--version"
   end
 
-  bottle do
-    cellar :any
-    root_url "https://github.com/brthor/homebrew-dotnet-cli/releases/download/1.0.0-preview2-003121"
-    sha256 "41da60a0fbab6b5ad981f72731957acc1b025b210d339af69e6daa74fd54bfe9" => :yosemite
-    sha256 "41da60a0fbab6b5ad981f72731957acc1b025b210d339af69e6daa74fd54bfe9" => :el_capitan
-  end
+  # bottle do
+  #   cellar :any
+  #   root_url "https://github.com/brthor/homebrew-dotnet-cli/releases/download/1.0.0-preview2-003121"
+  #   sha256 "41da60a0fbab6b5ad981f72731957acc1b025b210d339af69e6daa74fd54bfe9" => :yosemite
+  #   sha256 "41da60a0fbab6b5ad981f72731957acc1b025b210d339af69e6daa74fd54bfe9" => :el_capitan
+  # end
 end
